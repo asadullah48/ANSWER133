62e0afbe79095af4036d54928c7538f809731fc0
asadullah48 <asadullahshafique@hotmail.com>
2024-06-05 10:12:28 +0500

    no commit required

--- ans133.js
+++ ans133.js
@@ -1,10 +1,10 @@
 // Defines a JavaScript object
-var person = {
+var profile = {
     name: "Asadullah Shafique",
     age: 48,
     city: "Karachi",
 };
 // Converts the object into a JSON string
-var jsonString = JSON.stringify(person);
+var jsonString = JSON.stringify(profile);
 console.log(jsonString); // Outputs: {"name":"Alice","age":30,"city":"Wonderland"}
 // Demonstrates converting an object to a JSON string, making it easy to store or transmit.
--- ans133.ts
+++ ans133.ts
@@ -1,12 +1,12 @@
 // Defines a JavaScript object
-const person = {
+const profile = {
     name: "Asadullah Shafique",
     age: 48,
     city: "Karachi",
   };
   
   // Converts the object into a JSON string
-  const jsonString = JSON.stringify(person);
+  const jsonString = JSON.stringify(profile);
   
   console.log(jsonString); // Outputs: {"name":"Alice","age":30,"city":"Wonderland"}
   // Demonstrates converting an object to a JSON string, making it easy to store or transmit.
